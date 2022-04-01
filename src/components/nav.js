74ac3afb1210896c04649605729852903b6cc605
Idoko Agbo <idokojudge@gmail.com>
2022-04-01 10:29:05 -0400

    add nav component

---
+++ src/components/nav.js
@@ -0,0 +1,42 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Nav(props) {
+    return <nav className="navbar navbar-light bg-light navbar-expand-lg fixed-top">
+        <div className='container-fluid'>
+            <a
+                className="navbar-brand"
+                href="/"
+                rel="noopener noreferrer"
+            >
+                <img src={props.logo} width={30} height={30} className="d-inline-block align-top" alt='brand-logo' />
+                The Craft Logo
+            </a>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Gallery</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">FAQs</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Addons</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Order your Logo</a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>;
+}
+
+export default Nav;

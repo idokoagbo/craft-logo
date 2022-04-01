af3715e16efc44e81c018bf41e22e424a53489c5
Idoko Agbo <idokojudge@gmail.com>
2022-04-01 14:06:44 -0400

    add design section component

---
+++ src/components/designs.js
@@ -0,0 +1,33 @@
+import CircularBtn from './circularBtn';
+
+// frames
+import frame1 from '../assets/images/frames/1.png';
+import frame2 from '../assets/images/frames/2.png';
+import frame3 from '../assets/images/frames/3.png';
+import frame4 from '../assets/images/frames/4.png';
+import frame5 from '../assets/images/frames/5.png';
+
+function Designs() {
+
+    const frames = [frame1, frame2, frame3, frame4, frame5];
+
+    return <section className='mt-5'>
+        <div className='container'>
+            <h1 style={{ fontWeight: '700', fontSize: '48px', lineHeight: '96px' }} className="text-center">Create your unique logo</h1>
+            <div className='row mt-3 mb-5'>
+
+                {
+                    frames.map((frame) => {
+                        return <div className='col-md-4 col-6 mb-2'>
+                            <img src={frame} className="img-fluid" />
+                        </div>
+                    })
+                }
+            </div>
+
+            <CircularBtn text='View more designs' />
+        </div>
+    </section>
+}
+
+export default Designs;

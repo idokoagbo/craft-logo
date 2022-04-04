bd4a00a8bb71fc84eb3b09e9bd82a3e84ec75622
Idoko Agbo <idokojudge@gmail.com>
2022-04-04 06:07:23 -0400

    add link to btn component

--- src/components/circularBtn.js
+++ src/components/circularBtn.js
@@ -1,6 +1,6 @@
 function CircularBtn(props) {
     return <center>
-        <a href='' style={{ padding: "13px", borderRadius: '100px' }} className='btn btn-lg btn-dark'>
+        <a href={props.link} style={{ padding: "13px", borderRadius: '100px' }} className='btn btn-lg btn-dark'>
             <h3 style={{ fontWeight: '500', fontSize: '18px', lineHeight: '' }}>{props.text} &raquo;</h3>
         </a>
     </center>;

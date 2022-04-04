46dc7995b2147477ece617b40b9decd3f07d2ac7
Idoko Agbo <idokojudge@gmail.com>
2022-04-04 06:07:10 -0400

    add page routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Routes, Route }
+  from 'react-router-dom';
+
 import Index from './pages/index';
+import Gallery from './pages/gallery';
+import Order from './pages/order';
+import Addons from './pages/addons';
+import Review from './pages/review';
+
+
 
 function App() {
-  return (
-    <Index />
-  );
+  return <Router>
+    <Routes>
+      <Route exact path='/' element={<Index />} />
+      <Route exact path='/gallery' element={<Gallery />} />
+      <Route exact path='/order-logo' element={<Order />} />
+      <Route exact path='/addons' element={<Addons />} />
+      <Route exact path='/review-order' element={<Review />} />
+    </Routes>
+  </Router>;
 }
 
 export default App;

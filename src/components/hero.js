13b86658f4a1a1e772270226d2e905e2cebb5c1e
Idoko Agbo <idokojudge@gmail.com>
2022-04-05 08:48:53 -0400

    handle order logo form

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
 import sample1 from '../assets/images/samples/1.png'
 import sample2 from '../assets/images/samples/2.png'
 
 function Hero() {
+
+    const [logoName, setLogoName] = useState(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        window.location.href = document.location.href + `order-logo?name=${logoName}`
+    };
+
     return <div className="hero">
         <div className="container">
             <div className="row align-items-center">
@@ -10,12 +20,14 @@ function Hero() {
                         Handcrafted  Logo for Professionals
                     </h1>
                     <p style={{ fontWeight: '400', fontSize: '18px', lineHeight: '34px' }}>Get your calligraphic logo in just 3 steps and stand out among other professionals</p>
-                    <div class="input-group mb-3" style={{ width: '70%' }}>
-                        <input type="text" class="form-control" placeholder="Enter your logo name" aria-label="logo name" aria-describedby="basic-addon2" style={{ padding: 25, borderRadius: '180px 0px 0px 180px' }} />
-                        <div class="input-group-append">
-                            <button class="btn btn-dark" type="button" style={{ padding: 25, borderRadius: '0px 180px 180px 0px' }}>Order your logo $50 &raquo;</button>
+                    <form onSubmit={handleSubmit}>
+                        <div class="input-group mb-3" style={{ width: '70%' }}>
+                            <input type="text" onChange={(e) => setLogoName(e.target.value)} class="form-control" placeholder="Enter your logo name" aria-label="logo name" aria-describedby="basic-addon2" style={{ padding: 25, borderRadius: '180px 0px 0px 180px' }} required />
+                            <div class="input-group-append">
+                                <button class="btn btn-dark" type="submit" style={{ padding: 25, borderRadius: '0px 180px 180px 0px' }}>Order your logo $50 &raquo;</button>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
                 <div className="col-md-4">
                     <p><img src={sample1} alt="" /></p>

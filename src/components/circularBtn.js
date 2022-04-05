aa53766661dde562f01dc250eff86089820be076
Idoko Agbo <idokojudge@gmail.com>
2022-04-05 09:34:31 -0400

    hotfix: text placed in center

--- src/components/circularBtn.js
+++ src/components/circularBtn.js
@@ -1,7 +1,7 @@
 function CircularBtn(props) {
     return <center>
         <a href={props.link} style={{ padding: "13px", borderRadius: '100px' }} className='btn btn-lg btn-dark'>
-            <h3 style={{ fontWeight: '500', fontSize: '18px', lineHeight: '' }}>{props.text} &raquo;</h3>
+            <span style={{ fontWeight: '500', fontSize: '18px' }}>{props.text} &raquo;</span>
         </a>
     </center>;
 }

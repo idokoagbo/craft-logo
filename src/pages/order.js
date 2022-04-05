afb9416a21c5a56038830cdba0f64d936dfecd4c
Idoko Agbo <idokojudge@gmail.com>
2022-04-05 08:50:02 -0400

    add handle ordeer

--- src/pages/order.js
+++ src/pages/order.js
@@ -1,13 +1,44 @@
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
+import appLogo from '../assets/images/logo.svg';
 import '../assets/style/App.css';
-import logo from '../assets/images/logo.svg';
-import CircularBtn from '../components/circularBtn';
+
 
 function Order() {
+
+    const [logo, setLogo] = useState();
+    const [tag, setTag] = useState();
+    const [phone, setPhone] = useState();
+    const [email, setEmail] = useState();
+    const [confirm, setConfirm] = useState();
+    const [errorMessage, setErrorMessage] = useState();
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (email === confirm) {
+            sessionStorage.setItem('logoOrder', JSON.stringify({
+                logo: logo,
+                tag: tag,
+                phone: phone,
+                email: email,
+            }));
+
+            document.location.href = '/addons';
+            
+        } 
+        setErrorMessage('Email confirmation incorrect');
+
+
+    }
+
     return <div className='App'>
         <header className="App-header">
-            <Nav logo={logo} />
+            <Nav logo={appLogo} />
         </header>
 
         <section className='mt-5 mb-5'>
@@ -18,14 +49,18 @@ function Order() {
                         <div className='' style={{ padding: '20px', borderRaius: '9px', boxShadow: "0px 0px 40px rgba(239, 240, 241, 0.7)" }}>
                             <h1 style={{ fontWeight: '700', fontSize: '36px', lineHeight: '41.4px' }} className="text-center">Input your details</h1>
                             <h2 style={{ fontWeight: '500', fontSize: '18px', lineHeight: '21.09px' }} className="text-center">Check our FAQ</h2>
-                            <form>
+                            <form onSubmit={handleSubmit}>
 
-                                <InputElement text="Logo name" />
-                                <InputElement text="Logo tagline" />
-                                <InputElement text="Phone no." />
-                                <InputElement text="Email" />
-                                <InputElement text="Confirm email" />
-                                <p><CircularBtn text="Proceed" /></p>
+                                {errorMessage && <div className='alert alert-danger mb-3'><div className='container'>{errorMessage}</div></div>}
+
+                                <InputElement text="Logo name" value={searchParams.get("name")} todo={setLogo} />
+                                <InputElement text="Logo tagline" todo={setTag} />
+                                <InputElement text="Phone no." todo={setPhone} />
+                                <InputElement text="Email" todo={setEmail} />
+                                <InputElement text="Confirm email" todo={setConfirm} />
+                                <center>
+                                    <button style={{ padding: "13px", borderRadius: '100px' }} className="btn btn-lg btn-dark">Proceed &raquo;</button>
+                                </center>
                             </form>
                         </div>
                     </div>
@@ -39,10 +74,19 @@ function Order() {
 
 function InputElement(props) {
     return <div className="form-group">
-        <label for="exampleFormControlInput1" style={{ fontWeight: '700', fontSize: '18px', lineHeight: '20.7px', padding: '10px 0px' }}>{props.text}</label>
-        <input type="text" class="form-control mb-3" id="exampleFormControlInput1" placeholder="" style={{
-            backgroundColor: '#F5F8FA', padding: '10px', border: 0,
-        }} autoComplete="false" autoCorrect='false' />
+        <label style={{ fontWeight: '700', fontSize: '18px', lineHeight: '20.7px', padding: '10px 0px' }}>{props.text}</label>
+        <input
+            type="text"
+            className="form-control mb-3"
+            placeholder=""
+            value={props.value}
+            style={{
+                backgroundColor: '#F5F8FA', padding: '10px', border: 0,
+            }}
+            onChange={(e) => props.todo(e.target.value)}
+            autoComplete="false"
+            autoCorrect='false'
+            required />
     </div>;
 }
 
